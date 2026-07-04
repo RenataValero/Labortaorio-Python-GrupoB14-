b520d39530f534d521ee9a1fb74e2999b53948eb
Renata Valero <renata.valero.trenque@gmail.com>
2026-07-04 16:03:06 -0300

    Creo los txt necesarios

---
+++ LAB_PYTHON/sis_biblioteca.py
@@ -0,0 +1,17 @@
+"""
+Sistema de gestión de biblioteca - Escenario 7
+Trabajo Final Integrador Python
+"""
+
+import datetime
+import os
+
+ARCHIVO_USUARIOS = "usuarios.txt"
+ARCHIVO_LIBROS = "libros.txt"
+ARCHIVO_PRESTAMOS = "prestamos.txt"
+
+usuarios = {}
+libros = {}
+prestamos = []
+# Inicialización del proyecto - commit 1
+
